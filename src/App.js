067cab62de9dea4ddbfe936c527c3818aa7a7219
harshraj78 <nharshraj78@gmail.com>
2023-03-09 03:18:45 +0530

    fix: Fonts & Background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import Searchbox from './Searchbox';
+import { robots } from './robots';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// STATE:--> It means the discription of your App. 
+// PROPS:--> it is simply things that come out of state. 
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()); 
+        }) 
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <Searchbox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} /> 
+            </div>
+        );
+    }
 }
 
 export default App;
